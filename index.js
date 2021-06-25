a20949eeff9e2da3551381058ac52dd2d69b492e
Braden Boothe <braden@Bradens-MacBook-Pro.local>
2021-06-25 10:40:04 -0600

    fixed index and removed some code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const server = require("./server");
 const persist = require("./persist");
 const fs = require("fs");
-const background = require("./background");
+
 
 // define a port
 const portNumber = process.argv[2] || process.env.PORT || 8080;
@@ -14,16 +14,6 @@ persist.connect(function () {
     console.log(`Running Server on Port ${portNumber}`);
   });
 });
-
-// start our background process
-setInterval(() => {
-  background.myCountingProcess();
-}, 1000);
-
-// start our background process
-setInterval(() => {
-  background.myCleanUpProcess();
-}, 5000);
 
 
 // function readFile(filePath, callback) {
